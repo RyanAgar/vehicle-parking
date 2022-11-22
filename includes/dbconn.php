cae1e9f5a355aa95e032a8b4e9a56624ed59f33b
RyanAgar <ryanng6948@gmail.com>
2022-11-22 03:02:54 +0000

    first commit

---
+++ includes/dbconn.php
@@ -0,0 +1,6 @@
+<?php
+    $con=mysqli_connect("ryan-assignment2.mysql.database.azure.com", "ryan", "tG046250@", "vehicle-parking-db");
+    if(mysqli_connect_errno()){
+    echo "Connection Fail".mysqli_connect_error();
+    }
+  ?>
