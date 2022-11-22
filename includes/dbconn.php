2fd68d181ce23b24710f20d4888b4951ccb083b1
RyanAgar <ryanng6948@gmail.com>
2022-11-22 03:26:29 +0000

    jhj

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,16 +1,5 @@
 <?php
-    // DB credentials.
-define('DB_HOST','ryan-assignment2.mysql.database.azure.com');
-define('DB_USER','ryan');
-define('DB_PASS','tG046250@');
-define('DB_NAME','vehicle-parking-db');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem',PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, 'DigiCertGlobalRootCA.crt.pem', NULL, NULL);
+mysqli_real_connect($conn, "ryan-assignment2.mysql.database.azure.com", "ryan", "tG046250@", "vehicle-parking-db", 3306, MYSQLI_CLIENT_SSL);
   ?>
