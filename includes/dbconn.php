ed4cbd1c93fd42659c3d4a56fc4e571699866f64
RyanAgar <ryanng6948@gmail.com>
2022-11-24 02:55:57 +0000

    kbkhgg

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,5 +1,5 @@
 <?php
-    $con=mysqli_connect("ryan-assignment2.mysql.database.azure.com", "ryan", "tG046250@", "vehicle-parking-db");
+    $con=mysqli_connect("ryan-assignment2-sql.mysql.database.azure.com", "ryan", "tG046250@", "vehicle-parking-db");
     if(mysqli_connect_errno()){
     echo "Connection Fail".mysqli_connect_error();
     }
