fe0366acd097c1f5377694a649dc1e6d0bd46a55
RyanAgar <ryanng6948@gmail.com>
2022-11-24 03:42:12 +0000

    asdasd

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -7,7 +7,7 @@ define('DB_NAME','vehicle-parking-db');
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem',PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,));
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem',));
 }
 catch (PDOException $e)
 {
