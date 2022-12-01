b2ce28360bac33aa299c1f18290da99c5d44a427
RyanAgar <ryanng6948@gmail.com>
2022-12-01 11:52:29 +0800

    Update manage-vehicles.php
    asdasd

--- manage-vehicles.php
+++ manage-vehicles.php
@@ -124,18 +124,7 @@
 	</div>	<!--/.main-->
 	
 
-	<script>
-		window.onload = function () {
-		var chart1 = document.getElementById('line-chart').getContext('2d');
-		window.myLine = new Chart(chart1).Line(lineChartData, {
-		responsive: true,
-		scaleLineColor: "rgba(0,0,0,.2)",
-		scaleGridLineColor: "rgba(0,0,0,.05)",
-		scaleFontColor: "#c5c7cc"
-		});
-};
-	</script>
-		
+
 </body>
 <script src="js/jquery-1.11.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
